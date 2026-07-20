20b41ea4146153f079e12db19efb758bda2cc491
Jairus Otiko <jairusotiko@Jairuss-MacBook-Air.local>
2026-07-20 22:51:12 +0300

    chore:added route

---
+++ app/e5c37d9eaa6a8ad6f06a949328a19412c419c26166319cff0a4fbe2f66566498.txt/route.ts
@@ -0,0 +1,10 @@
+export function GET() {
+  return new Response(
+    "e5c37d9eaa6a8ad6f06a949328a19412c419c26166319cff0a4fbe2f66566498",
+    {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    },
+  );
+}
